3d923d4c01a8e93239e82d015e8c460702aa7f45
Suraj-Sarkar3402 <131189034+Suraj-Sarkar3402@users.noreply.github.com>
2023-04-26 23:45:45 +0530

    Add files via upload

--- Summation.java
+++ Summation.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+public class Summation{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the integer one by one: \n");
+		int sum=0;
+		while(true) {
+		    int num =sc.nextInt();
+			if(num<0){
+				break;
+			}
+			sum=sum+num;
+		}
+		System.out.println("The summation of all positive integer you entered is: "+sum);
+	}
+}
